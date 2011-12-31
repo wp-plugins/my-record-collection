58f0dcca976dda9f984e6ad9541e64a1fddd96e5
volmar <volmar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-31 14:17:53 +0000

    updated uninstall file (ready for 1.0.0)
    git-svn-id: https://plugins.svn.wordpress.org/my-record-collection/trunk@482772 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -2,28 +2,9 @@
 if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
     exit();
 	
-	function rmdir_recursive($dir) {
-		$files = scandir($dir);
-		array_shift($files);    // remove '.' from array
-		array_shift($files);    // remove '..' from array
-	   
-		foreach ($files as $file) {
-			$file = $dir . '/' . $file;
-			if (is_dir($file)) {
-				rmdir_recursive($file);
-				rmdir($file);
-			} else {
-				unlink($file);
-			}
-		}
-		rmdir($dir);
-	}
-
-	rmdir_recursive(get_option('mrc_upload_dir'));
 	//DELETING THE WP OIPTIONS CREATED
-	delete_option('mrc_display_mode');
-	delete_option('mrc_upload_dir');
-	delete_option('mrc_xml_file');
+	delete_option('mrc_username');
+	delete_option('mrc_settings');
 
 	//DELETING THE DATABASE TABLE
 	global $wpdb;
