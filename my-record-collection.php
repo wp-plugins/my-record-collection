621169460006490686863b7a74348e613fa2d375
volmar <volmar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-07 21:11:04 +0000

    correct active version in main php-file
    git-svn-id: https://plugins.svn.wordpress.org/my-record-collection/trunk@554550 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- my-record-collection.php
+++ my-record-collection.php
@@ -4,7 +4,7 @@
 	Plugin URI: http://myrecordcollection.arvid.nu/
 	Description: Plugin for displaying your recordcollection on Discogs.com in your blog
 	Author: Arvid Sollenby
-	Version: 1.0.0
+	Version: 1.0.2
 	Author URI: http://www.arvid.nu
 	*/
 
